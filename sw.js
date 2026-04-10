dfb4699ecfea6cd6ced773c6f8016d3e54c6f543
komaitoreha <komaitoreha@gmail.com>
2026-04-10 12:52:50 +0900

    Initial commit: Wall Grid - climbing route creation tool

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'wall-grid-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
